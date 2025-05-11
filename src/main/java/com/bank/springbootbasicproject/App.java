27e7da3b5149dcb6261d0bf953051137c44af13d
karthikeya80 <karthikeya80@gmail.com>
2025-05-11 22:31:31 +0530

    guys message

--- src/main/java/com/bank/springbootbasicproject/App.java
+++ src/main/java/com/bank/springbootbasicproject/App.java
@@ -3,6 +3,7 @@ package com.bank.springbootbasicproject;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("springboot1...!");
+		System.out.println("Guys welcome...");
 
 	}
 }
