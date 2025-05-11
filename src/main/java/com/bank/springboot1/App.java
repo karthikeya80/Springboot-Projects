f74313320f4dc6c3b39b077371c77cc31b04bf82
karthikeya80 <karthikeya80@gmail.com>
2025-05-11 20:21:09 +0530

    two

--- src/main/java/com/bank/springboot1/App.java
+++ src/main/java/com/bank/springboot1/App.java
@@ -1,9 +1,9 @@
 package com.bank.springboot1;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
 }
