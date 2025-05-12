2a46b43e74ad5ec9603fa7471f24c631e146b868
karthikeya80 <karthikeya80@gmail.com>
2025-05-12 10:05:02 +0530

    controller class added

---
+++ src/main/java/com/bank/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.bank.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bank.springbootbasicproject.MyFirstSpringBootProject;
+
+public class StudentController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyFirstSpringBootProject.class);
+
+
+}
