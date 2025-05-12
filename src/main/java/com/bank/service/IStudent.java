7b97b652a0188389f15ecad6b8252d6501dd7cab
karthikeya80 <karthikeya80@gmail.com>
2025-05-12 11:22:02 +0530

    ListOfStudent method addeda and implememnted

--- src/main/java/com/bank/service/IStudent.java
+++ src/main/java/com/bank/service/IStudent.java
@@ -1,7 +1,12 @@
 package com.bank.service;
 
+import java.util.List;
+
+import com.bank.entity.Student;
+
 public interface IStudent {
 
 	public void getStudentdata();
+	public List<Student> listOfStudentsData();
 
 }
--- src/main/java/com/bank/service/StudentImpl.java
+++ src/main/java/com/bank/service/StudentImpl.java
@@ -1,8 +1,11 @@
 package com.bank.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bank.entity.Student;
 import com.bank.springbootbasicproject.MyFirstSpringBootProject;
 
 public class StudentImpl implements IStudent {
@@ -10,8 +13,13 @@ public class StudentImpl implements IStudent {
 	private static final Logger logger = LoggerFactory.getLogger(MyFirstSpringBootProject.class);
 
 	public void getStudentdata() {
-		logger.info("getStudentdata started successfully...");
+		logger.info("getStudentdata() started successfully...");
 
+	}
+
+	public List<Student> listOfStudentsData() {
+		logger.info("listOfStudentsData() started successfully...");
+		return null;
 	}
 
 }
